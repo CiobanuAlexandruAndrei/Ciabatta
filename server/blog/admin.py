151bc974f72103e661107ae2a707d16268573489
Alexandru Andrei Ciobanu <96230391+CiobanuAlexandruAndrei@users.noreply.github.com>
2024-07-19 16:38:40 +0100

    Removal of Django REST project version

--- server/blog/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(APIUsed)
-admin.site.register(APICostRecord)
-admin.site.register(Keyword)
-admin.site.register(KeywordData)
-admin.site.register(KeywordCluster)
--- server/security/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from security.models import Profile
-
-admin.site.register(Profile)
